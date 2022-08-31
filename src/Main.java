e489d833e0f667b03ee13296a2b9ddbf34852df5
Vishv Deshwal <vishavdeshwal@gmail.com>
2022-08-31 12:21:19 +0530

    Created a new array named arr..

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {5,2,6,8,4,7,5,1};
+        System.out.println(Arrays.toString(arr));
+    }
+}
